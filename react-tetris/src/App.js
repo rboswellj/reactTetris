cefb573503913e347fa1f7999f8cbc730b3935af
rboswellj <rboswellj@gmail.com>
2022-01-02 17:29:25 -0500

    added initial message popup

--- react-tetris/src/App.js
+++ react-tetris/src/App.js
@@ -2,6 +2,7 @@ import GridBoard from './components/GridBoard'
 import NextBlock from './components/NextBlock';
 import ScoreBoard from './components/ScoreBoard';
 import Controls from './components/Controls';
+import MessagePopup from './components/MessagePopup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
   );
 }
