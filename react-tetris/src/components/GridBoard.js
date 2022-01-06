5f13d6a9df549c1af4cbb81b5990ea93857ee7f0
rboswellj <rboswellj@gmail.com>
2022-01-06 00:05:11 -0500

    added connection for grid board.

--- react-tetris/src/components/GridBoard.js
+++ react-tetris/src/components/GridBoard.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import GridSquare from './GirdSquare';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils'
 
 export default function GridBoard(props) {
-    /* generate grid */
-    const gridWidth = 10;
-    const gridHeight = 18;
-    const grid = [];
+    const game = useSelector((state) => state.game)
+    const { grid, shape, rotation, x, y, isRunning, speed } = game
 
-    for (let row = 0; row < gridHeight; row++) {
-        grid.push([]);
-        for (let col = 0; col < gridWidth; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color = "1" />)
-        }
-    }
+    const block = shapes[shape][rotation]
+  const blockColor = shape
+  // map rows
+  const gridSquares = grid.map((rowArray, row) => {
+    // map columns
+    return rowArray.map((square, col) => {
+      // Find the block x and y on the shape grid
+      // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
+      const blockX = col - x
+      const blockY = row - y
+      let color = square
+      // Map current falling block to grid.
+      // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
+      if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+        color = block[blockY][blockX] === 0 ? color : blockColor
+      }
+      // Generate a unique key for every block
+      const k = row * grid[0].length + col;
+      // Generate a grid square
+      return <GridSquare
+              key={k}
+              color={color} />
+    })
+  })
 
     return (
         <div className='grid-board'>
-            {grid}
+            {gridSquares}
         </div>
-    );
+    )
 }
--- react-tetris/src/components/NextBlock.js
+++ react-tetris/src/components/NextBlock.js
@@ -1,21 +1,16 @@
 import React from "react";
 import GridSquare from "./GirdSquare";
 import { useSelector } from 'react-redux';
-import { shapes } from '../utils/index.js';
+import { shapes } from '../utils/';
 
 export default function NextBlock(props) {
     const nextShape = useSelector((state) => state.game.nextShape);
-    const box = shapes[nextShape][0]; //gets first rotation
+    const box = shapes[nextShape][0];
+    
     const grid = box.map((rowArray, row) => {
         return rowArray.map((square, col) => {
             return <GridSquare key={`${row}${col}`} color = {square} />
         });
     });
-
-    return (
-        <div className="next-block">
-            { grid }
-        </div>
-    )
 }
 
