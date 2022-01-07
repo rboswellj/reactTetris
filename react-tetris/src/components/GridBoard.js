a9ab9790c26c505611c40294b070f5f7f130e437
rboswellj <rboswellj@gmail.com>
2022-01-07 16:49:41 -0500

    timer created

--- react-tetris/src/components/GridBoard.js
+++ react-tetris/src/components/GridBoard.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import GridSquare from './GirdSquare';
-import { useSelector } from 'react-redux';
-import { shapes } from '../utils'
+import { useSelector, useDispatch } from 'react-redux';
+import { shapes } from '../utils';
+import { moveDown } from '../actions';
 
 export default function GridBoard(props) {
-    const game = useSelector((state) => state.game)
-    const { grid, shape, rotation, x, y, isRunning, speed } = game
-
-    const block = shapes[shape][rotation]
+  const requestRef = useRef();
+  const lastUpdateTimer = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
+  const game = useSelector((state) => state.game)
+  const { grid, shape, rotation, x, y, isRunning, speed } = game
+  const block = shapes[shape][rotation]
   const blockColor = shape
+
   // map rows
   const gridSquares = grid.map((rowArray, row) => {
     // map columns
@@ -32,6 +37,28 @@ export default function GridBoard(props) {
     })
   })
 
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    } 
+    lastUpdateTimeRef.current = time;
+  }
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [isRunning])
+  
     return (
         <div className='grid-board'>
             {gridSquares}
