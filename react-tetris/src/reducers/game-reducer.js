4681b8fc2be4c4e03f4472ca16f51f0be0bc57d6
rboswellj <rboswellj@gmail.com>
2022-01-07 16:24:44 -0500

    Added move down

--- react-tetris/src/reducers/game-reducer.js
+++ react-tetris/src/reducers/game-reducer.js
@@ -2,8 +2,9 @@ import {
     MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
     PAUSE, RESUME, RESTART, GAME_OVER
   } from '../actions';
+
   import { 
-      defaultState, nextRotation, canMoveTo 
+      defaultState, nextRotation, canMoveTo, addBlockToGrid, checkRows, randomShape 
     } from '../utils';
 
     const gameReducer = (state = defaultState(), action) => {
@@ -13,7 +14,7 @@ import {
             case ROTATE:
                 const newRotation = nextRotation(shape,rotation);
                 if(canMoveTo(shape, grid, x, y, newRotation)) {
-                    return {...state,rotation,newRotation};
+                    return {...state, rotation: newRotation};
                 }
               return state;
 
@@ -30,8 +31,34 @@ import {
                 }
                 return state;
 
-            case MOVE_DOWN:
-                return state;
+                case MOVE_DOWN:
+                    // Get the next potential Y position
+                    const maybeY = y + 1
+                    // Check if the current block can move here
+                    if (canMoveTo(shape, grid, x, maybeY, rotation)) {
+                        // If so move the block
+                        return { ...state, y: maybeY }
+                    }
+                    // If not place the block
+                    const newGrid = addBlockToGrid(shape, grid, x, y, rotation)
+                    // reset some things to start a new shape/block
+                    const newState = defaultState();
+                    newState.grid = newGrid;
+                    newState.shape = nextShape;
+                    newState.nextShape = randomShape();
+                    newState.score = score;
+                    newState.isRunning = isRunning;
+                  
+                    if (!canMoveTo(nextShape, newGrid, 0, 4, 0)) {
+                      // Game Over
+                      console.log("Game Should be over...");
+                      newState.shape = 0;
+                      return { ...state, gameOver: true }
+                    }
+                    // Update the score based on if rows were completed or not
+                    newState.score = score + checkRows(newGrid);
+                  
+                    return newState;
 
             case RESUME:
                 return state;
--- react-tetris/src/utils/index.js
+++ react-tetris/src/utils/index.js
@@ -166,38 +166,71 @@ export const defaultState = () => {
 // rotation can't exceed the last index of the the rotations for the given shape.
 
 export const nextRotation = (shape, rotation) => {
-    return (rotation + 1) % shapes[shape].length;
+    return (rotation + 1) % shapes[shape].length
 }
 
 export const canMoveTo = (shape, grid, x, y, rotation) => {
-    const currentShape = shapes[shape][rotation];
-    //loop through all rows and columns of the shape
-    for(let row=0; row < currentShape.length; row++) {
-        for(let col=0; col < currentShape[row].length; col++) {
-            // look for 1
-            if(currentShape[row][col] !== 0 ) {
+    const currentShape = shapes[shape][rotation]
+    // Loop through all rows and cols of the **shape**
+    for (let row = 0; row < currentShape.length; row++) {
+        for (let col = 0; col < currentShape[row].length; col++) {
+            // Look for a 1 here
+            if (currentShape[row][col] !== 0) {
                 // x offset on grid
-                const proposedX = col + x;
-                //y offset on grid
-                const proposedY = row + y;
-                if(proposedY < 0) {
-                    continue;
+                const proposedX = col + x
+                // y offset on grid
+                const proposedY = row + y
+                if (proposedY < 0) {
+                    continue
                 }
-                //get row on grid
-                const possibleRow = grid[proposedY];
-                //check row exists
-                if(possibleRow) {
-                    // Check if this column in the row is undefined, 
-                    //if it's off the edges, 0, and empty
-                    if(possibleRow[proposedX] === undefined || possibleRow[proposedX] !== 0) {
+                // Get the row on the grid
+                const possibleRow = grid[proposedY]
+                // Check row exists
+                if (possibleRow) {
+                    // Check if this column in the row is undefined, if it's off the edges, 0, and empty
+                    if (possibleRow[proposedX] === undefined || possibleRow[proposedX] !== 0) {
                         // undefined or not 0 and it's occupied we can't move here.
-                        return false;
+                        return false
                     }
                 } else {
-                    return false;
+                    return false
                 }
             }
         }
     }
-    return true;
+    return true
+}
+
+// Adds current shape to grid
+export const addBlockToGrid = (shape, grid, x, y, rotation) => {
+    // Get the block array
+    const block = shapes[shape][rotation];
+    // Copy the grid
+    const newGrid = [...grid];            
+    // Map the Block onto the grid                                                           
+    for (let row = 0; row < block.length; row++) {
+        for (let col = 0; col < block[row].length; col++) {
+            if (block[row][col]) {
+                newGrid[row + y][col + x] = shape;
+            }
+        }
+    }
+    return newGrid;
+}
+
+// Checks for completed row and scores points
+export const checkRows = (grid) => {
+    // Points increase for each complete row
+    const points = [0, 40, 100, 300, 1200];
+    let completedRows = 0;
+    for (let row = 0; row < grid.length; row++) {
+        // No empty cells = can't find 0 in the row
+        if (grid[row].indexOf(0) === -1) {
+            completedRows += 1;
+            // Remove row and add new empty one at top
+            grid.splice(row, 1);
+            grid.unshift(Array(10).fill(0));
+        }
+    }
+    return points[completedRows];
 }
